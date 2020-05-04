deffa145228651ad85acf28216c71c58ae778812
LostzSoul <61343588+LostzSoul@users.noreply.github.com>
2020-05-04 19:31:38 +0700

    กฟกฟ

---
+++ Assets/RIP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RIP : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public GameObject goalGameObject;
+    
+    private void OnMouseDrag()
+    {
+        
+        Vector3 mouse =  Input.mousePosition;
+        mouse.z = transform.position.z - Camera.main.transform.position.z;
+        mouse = Camera.main.ScreenToWorldPoint(mouse);
+        transform.position = new Vector3(mouse.x,mouse.y,mouse.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(goalGameObject) )
+        {
+            Debug.Log("win");
+        }
+
+    }
+
+}
